9d1d911b56610ae67d605d08039a16ae2eda5e04
cassandra-mn <arthenciac@gmail.com>
2022-03-24 13:31:14 -0300

    filmes vindo da API

--- src/Componentes/Filmes/index.js
+++ src/Componentes/Filmes/index.js
@@ -1,55 +1,38 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { Link } from "react-router-dom";
-import image1 from "./../../assets/image 3.png";
-import image2 from "./../../assets/image 7.png";
+import axios from 'axios';
+
 import "./style.css";
 
 export default function Filmes() {
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+        const requisicao = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
+
+        requisicao.then(resposta => {
+            setFilmes(resposta.data);
+            console.log(resposta.data);
+        });
+    }, []);
+
     return (
         <div className="Filmes">
             <section>
                 <p>Selecione o filme</p>
             </section>
             <main className="filmes">
-                <Link to="/filme/37">
-                    <div className="borda">
-                        <img src={image1} />
-                    </div>
-                </Link>
-                <Link to="/filme/37">
-                    <div className="borda">
-                        <img src={image2} />
-                    </div>
-                </Link>
-                <Link to="/filme/37">
-                    <div className="borda">
-                        <img src={image1} />
-                    </div>
-                </Link>
-                <Link to="/filme/37">
-                    <div className="borda">
-                        <img src={image2} />
-                    </div>
-                </Link>
-                <Link to="/filme/37">
-                    <div className="borda">
-                        <img src={image1} />
-                    </div>
-                </Link>
-                <Link to="/filme/37">
-                    <div className="borda">
-                        <img src={image2} />
-                    </div>
-                </Link>
-                <Link to="/filme/37">
-                    <div className="borda">
-                        <img src={image1} />
-                    </div>
-                </Link>
-                <Link to="/filme/37">
-                    <div className="borda">
-                        <img src={image2} />
-                    </div>
-                </Link>
+                {filmes.map(filme => {
+                    const {id, posterURL} = filme;
+                    return (
+                        <Link to="/filme/37">
+                            <div className="borda">
+                                <img src={posterURL} />
+                            </div>
+                        </Link>
+                    );
+                })}
             </main>
         </div>
     )
