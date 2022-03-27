c4e92bf1c77755038b0452d854360425e988d31f
cassandra-mn <arthenciac@gmail.com>
2022-03-27 14:11:17 -0300

    assentos selecionando e descelecionando

--- src/Componentes/Confirmacao/index.js
+++ src/Componentes/Confirmacao/index.js
@@ -1,8 +1,27 @@
+import { useParams, useNavigate, useLocation } from 'react-router-dom';
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 import "./style.css";
 
-export default function Confirmacao(props) {
-    console.log(props);
+export default function Confirmacao(route) {
+    const [confirmar, setConfirmar] = useState({});
+    const location = useLocation();
+    // const { ids, name, cpf } = route.params;
+
+    // useEffect(() => {
+    //     const requisicao = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/seats/book-many");
+
+    //     requisicao.then(resposta => setConfirmar(resposta.data))
+    //         .catch(erro => console.log(erro.response));
+    // }, []);
+
+    // console.log(ids);
+    // console.log(name);
+    // console.log(cpf);
+
+    console.log(location.search);
+    
     return (
         <div className="Confirmacao">
             <section>
--- src/Componentes/Sessao/index.js
+++ src/Componentes/Sessao/index.js
@@ -1,27 +1,15 @@
+import { useParams, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { Link } from "react-router-dom";
 import axios from 'axios';
 import "./style.css";
 
 export default function Sessao() {
     const [sessoes, setSessoes] = useState([]);
-    const [selecionar, setSelecionar] = useState([]);
-    const [nome, setNome] = useState([]);
-    const [cpf, setCpf] = useState([]);
+    const [assentos, setAssentos] = useState([]);
+    const [nome, setNome] = useState("");
+    const [cpf, setCpf] = useState("");
     const { idSessao } = useParams();
-    const ids = [...new Set(selecionar)];
-    const dadosDoPedido = {
-        ids: ids,
-        name: nome,
-        cpf: cpf
-    }
-
-    function dados(e) {
-        e.preventDefault();
-
-        const requisicao = axios.post("https://mock-api.driven.com.br/api/v5/cineflex/seats/book-many", dadosDoPedido);
-    }
+    const navigate = useNavigate();
 
     useEffect(() => {
         const requisicao = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${idSessao}/seats`);
@@ -30,10 +18,24 @@ export default function Sessao() {
             .catch(erro => console.log(erro.response));
     }, []);
 
-    function select(sessao) {
-        const { name, isAvailable } = sessao;
+    function enviarDados(e) {
+        e.preventDefault();
+        const requisicao = axios.post("https://mock-api.driven.com.br/api/v5/cineflex/seats/book-many", {
+            ids: assentos,
+            name: nome,
+            cpf: cpf
+        });
 
-        isAvailable ? setSelecionar([...selecionar, name]) : alert("Esse assento não está disponível");
+        requisicao.then(resposta => {
+            navigate("/sucesso", {
+                ids: assentos,
+                name: nome,
+                cpf: cpf
+            });
+            console.log(assentos);
+            console.log(nome);
+            console.log(cpf);
+        }).catch(erro => console.log(erro.response));
     }
 
     if (sessoes.length === undefined) {
@@ -48,14 +50,9 @@ export default function Sessao() {
                 </section>
                 <main>
                     <div className="assentos">
-                        {seats.map(sessao => {
-                            const { id, name, isAvailable } = sessao;
-                            let css = isAvailable ? "assento" : "assento indisponivel";
-
-                            return (
-                                <div key={id} className={css} onClick={() => select(sessao)}>{name}</div>
-                            );
-                        })}
+                        {seats.map(sessao => 
+                            <Seat seat={sessao} assentos={assentos} setAssentos={setAssentos}/>
+                        )}
                     </div>
                     <div className="assentos legendas">
                         <div className="legenda">
@@ -72,21 +69,18 @@ export default function Sessao() {
                         </div>
                     </div>
                 </main>
-                <aside>
-                    <form onSubmit={dados}>
-                        <div className="informacoes">
-                            <label>Nome do comprador:</label>
-                            <input type="text" required placeholder="Digite seu nome..." value={nome} onChange={(event) => setNome(event.target.value)} />
-                        </div>
-                        <div className="informacoes">
-                            <label>CPF do comprador:</label>
-                            <input type="number" required placeholder="Digite seu CPF..." value={cpf} onChange={(event) => setCpf(event.target.value)} />
-                        </div>
-                    </form>
-                </aside>
-                <Link to="/sucesso" >
+                <form onSubmit={enviarDados}>
+                    <div className="informacoes">
+                        <label>Nome do comprador:</label>
+                        <input type="text" required placeholder="Digite seu nome..." value={nome} onChange={(event) => setNome(event.target.value)} />
+                    </div>
+                    <div className="informacoes">
+                        <label>CPF do comprador:</label>
+                        <input type="number" required placeholder="Digite seu CPF..." value={cpf} onChange={(event) => setCpf(event.target.value)} />
+                    </div>
                     <button type="submit">Reservar assento(s)</button>
-                </Link>
+                </form>
+
                 <footer>
                     <div className="borda">
                         <img src={posterURL} />
@@ -102,4 +96,24 @@ export default function Sessao() {
     else {
         return <></>
     }
+}
+
+function Seat({seat, assentos, setAssentos}) {
+    const {id, name, isAvailable} = seat;
+    const [selecionado, setSelecionado] = useState(false);
+    
+    return isAvailable ? (
+        <div key={id} className={selecionado ? "assento selecionado" : "assento"} 
+            onClick={() => {
+                setSelecionado(!selecionado);
+                selecionado ? setAssentos(assentos.filter(assento => assento !== name)) : setAssentos([...assentos, name]);
+            }}>
+            {name}
+        </div>
+    ) : (
+        <div key={id} className="assento indisponivel" 
+            onClick={() => alert("Esse assento não está disponível")}>
+            {name}
+        </div>
+    );
 }
